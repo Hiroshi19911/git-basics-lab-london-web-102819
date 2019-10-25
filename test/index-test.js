f64fa26bfa42fc333d29773d02d84aac4e49f7a8
Hiroshi Sipahioglu <hiroshis91@icloud.com>
2019-10-25 12:39:58 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -21,14 +21,6 @@ describe( 'this lab', () => {
 
 describe( 'the local repository', () => {
 
-  it( 'has README.md as a tracked file', () => {
-   chai.assert.pathExists( './my-repository/.git/index', 'no files are being tracked.  Use "git add ." to track all files in this repo' );
-  } )
-
- it( 'has at least one commit', () => {
-    chai.assert.isDirectory( './my-repository/.git/logs', 'no commits were found.  Use "git commit -m" followed by a message to create a commit' );
-   } )
-
 
 
  } )
